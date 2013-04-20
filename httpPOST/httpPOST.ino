28e5701268a6b30c3c5447d4c1994c4a9bbe7edd
flg8r96 <flg8r96@gmail.com>
2013-04-20 08:28:43 -0700

    initial post test - not working

--- httpPOST/httpPOST.ino
+++ httpPOST/httpPOST.ino
@@ -1,5 +1,12 @@
+#include <Ethernet.h>
+#include <WString.h>
+
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+byte ip[] = { 192, 168, 2, 12};  //IP address of the Ethernet Shield
+
 IPAddress server(10,0,0,138);
-String PostData = "someDatatoPost";
+// String PostData = "someDatatoPost";
+String PostData = "{'name':'joe'}";
 
 if (client.connect(server, 80)) {
   client.println("POST /Api/AddParking/3 HTTP/1.1");
